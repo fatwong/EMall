b516f31af4c648b8dc47aeef5e57188de4ca4ec0
fatwong <isaacwong0108@gmail.com>
2017-11-28 22:41:47 +0800

    demo

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+          <div class="footer__region">
+            <span>Region</span>
+            <select class="footer__region__select">
+              <option value="zh-CN">中国</option>
+              <option value="en-US">US</option>
+            </select>
+          </div>
+          <div class="footer__secondary__nav">
+            <span>Copyright © 2017 Isaac All Rights Reserved.</span>
+            <a href="#">
+              About Us
+            </a>
+            <a href="#">
+              Terms &amp; Conditions
+            </a>
+            <a href="#">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
